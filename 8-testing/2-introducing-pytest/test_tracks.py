37f2b05296e743fc490a93dd45d6d06954464dfe
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-03-19 17:21:21 -0400

    update

--- 8-testing/2-introducing-pytest/test_tracks.py
+++ 8-testing/2-introducing-pytest/test_tracks.py
@@ -1,17 +1,18 @@
-# def clean_track(track):
-#     return track.split(' - ')
+def clean_track(track):
+    return track.split(' - ')
 
 
-# def test_clean_track():
-#     track_name = "When I'm Sixty-Four - Remix"
-#     assert clean_track(track_name) == "When I'm Sixty-Four"
+def test_clean_track():
+    track_name = "When I'm Sixty-Four - Remix"
+    assert clean_track(track_name) == "When I'm Sixty-Four"
 
-    
+songs = [{'rank': 1, 'song': 'Like a Rolling Stone', 'artist': 'Bob Dylan', 'year': 1965},  {'rank': 2, 'song': 'Satisfaction', 'artist': 'The Rolling Stones', 'year': 1965}, {'rank': 5, 'song': 'Respect', 'artist': 'Aretha Franklin', 'year': 1967}]
+
+
 def find_song(songs, name):
     found_song = [song for song in songs if song['song'] == name]
     return next(iter(found_song), 'none')
 
-songs = [{'rank': 1, 'song': 'Like a Rolling Stone', 'artist': 'Bob Dylan', 'year': 1965},  {'rank': 2, 'song': 'Satisfaction', 'artist': 'The Rolling Stones', 'year': 1965}, {'rank': 5, 'song': 'Respect', 'artist': 'Aretha Franklin', 'year': 1967}]
 
 def test_find_song_returns_song_dict():
     found_song = find_song(songs, 'Like a Rolling Stone')
