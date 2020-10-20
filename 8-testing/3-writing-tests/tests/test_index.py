695d6a5cae4c9292faa26f3d31ffd6e5a87b989d
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-10-20 11:33:52 -0400

    update coercing movies lab

--- 8-testing/3-writing-tests/tests/test_index.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import sys
-sys.path.append(".")
-
-from writingcode.index import *
-
-def test_hello_world():
-    assert hello_world() == 'hello world alsjs'
--- 8-testing/9-practice/practice/index.py
+++
@@ -1,3 +0,0 @@
-import pdb
-def params_string(params):
-    return '&'.join([f'{k}={v}' for k, v in params.items()])
